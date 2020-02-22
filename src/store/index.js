5f73cc4bfe9f32761d1af01b6ce7f1626a54a4e5
gary <yiso05255@gmail.com>
2020-02-22 19:11:26 +0800

    module demo finished

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,78 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+const countModule = {
+    namespaced: true,
+    state: { count: 0 },
+    mutations: {
+      increment(state, payload) {
+        state.count++
+      },
+      decrement(state) {
+        state.count--
+      },
+      reset(state) {
+        state.count = 0
+      }
+    },
+    getters: { // 當讀取屬性值時自動呼叫並返回屬性值
+      evenOrOdd(state) {
+        return state.count % 2 === 0 ? "偶數" : "奇數";
+      }
+    },
+    actions: {
+      increment: ({ commit }) => commit('increment'),
+      decrement: ({ commit }) => commit('decrement'),
+      reset: ({ commit }) => commit('reset'),
+      incrementIfOdd({ commit, state }) { // 帶條件的action
+        if (state.count % 2 === 1) {
+          commit('increment')
+        }
+      },
+      incrementAsync({ commit }) { //非同步的action
+        setInterval(() => {
+          commit('increment')
+        }, 1000)
+      }
+    },
   }
-})
+  
+  const moduleB = {
+    namespaced: true,
+    state: {
+      count: 0,
+      title: "I am moduleB"
+    },
+    mutations: {
+      increment(state, payload) {
+        state.count++
+      },
+      decrement(state) {
+        state.count--
+      },
+      reset(state) {
+        state.count = 0
+      }
+    },
+    getters: {},
+    actions: {
+      increment: ({ commit }) => commit('increment'),
+      decrement: ({ commit }) => commit('decrement'),
+      reset({ dispatch, commit }) {
+        commit('reset')
+        return dispatch('incrementAsync').then(() => {})
+      },
+      incrementAsync({ commit }) { //非同步的action
+        setInterval(() => {
+          commit('increment')
+        }, 1000)
+      }
+    }
+  
+  }
+  export default new Vuex.Store({
+    modules: {
+      a: countModule,
+      b: moduleB
+    }
+  })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <h1>Vuex Demo</h1>
+
+  <div class="ui equal width grid">
+    <div class="row">
+      <div class="column">
+        <h2>moduleA</h2>
+        <p>目前點擊的次數:<a class="ui blue circular label">{{ $store.state.a.count }}</a></p>
+        <div class="ui icon buttons">
+          <button class="ui medium button" @click="$store.dispatch('a/increment')"><i class="align plus icon"></i></button>
+          <button class="ui medium button" @click="$store.dispatch('a/decrement')"><i class="align minus icon"></i></button>
+            <button class="ui red button" @click="$store.dispatch('a/reset')">歸零</button>
+            <button class="ui button" @click="$store.dispatch('a/incrementIfOdd')">if是奇數+1</button>
+            <button class="ui green button" @click="$store.dispatch('a/incrementAsync')">同步累加</button>
+        </div>
+      </div>
+      <div class="column">
+        <h2>moduleB</h2>
+        <p>目前點擊的次數:<a class="ui blue circular label">{{ $store.state.b.count }}</a></p>
+        <div class="ui icon buttons">
+          <button class="ui medium button" @click="$store.dispatch('b/increment')"><i class="align plus icon"></i></button>
+            <button class="ui medium button" @click="$store.dispatch('b/decrement')"><i class="align minus icon"></i></button>
+            <button class="ui red button" @click="$store.dispatch('b/reset')">歸零</button>
+            <button class="ui green button" @click="$store.dispatch('b/incrementAsync')">同步累加</button>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  computed: mapGetters([
+    'evenOrOdd',
+  ]),
+  methods: mapActions([
+
+  ])
 }
 </script>
